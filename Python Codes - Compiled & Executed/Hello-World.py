78532fabe424a8d96608799d4688279b6df35b62
Prathamesh <prathamesh7111@gmail.com>
2026-02-14 18:02:20 +0530

    Committing First Python Program

---
+++ Python Codes - Compiled & Executed/Hello-World.py
@@ -0,0 +1,5 @@
+#Printing to the Console in Python
+#The print function is used to print the message to the screen.
+
+print("Hello World Program")
+#The content between the double quotes is referred to as strings.
